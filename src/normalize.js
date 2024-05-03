627a1b88684f1874218f6e647a3da0284634ae8d
Roman Astashevich <irqmen@gmail.com>
2024-05-03 14:00:57 +0300

    Fix sorting `envVariables` issue

--- src/normalize.js
+++ src/normalize.js
@@ -1,7 +1,7 @@
 export function normalize(args, envVariables, isWindows) {
   return args.map((arg) => {
     Object.keys(envVariables)
-      .sort((x, y) => y.length - y.length) // NOTE: sort by descending length to prevent partial replacement
+      .sort((x, y) => y.length - x.length) // NOTE: sort by descending length to prevent partial replacement
       .forEach((key) => {
         const regex = new RegExp(`\\$${key}|%${key}%`, "ig");
         arg = arg.replace(regex, envVariables[key]);
