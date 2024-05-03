e1e86d87ed84b9be39019b69fbc1661093925a85
Roman Astashevich <irqmen@gmail.com>
2024-05-03 13:58:15 +0300

    Create simple test cases for the `normalize`

--- src/normalize.test.js
+++ src/normalize.test.js
@@ -1,8 +1,39 @@
-import { describe, expect, test } from "@jest/globals";
-import { normalize } from "./normalize";
+import { describe, expect, test } from '@jest/globals';
+import { normalize } from './normalize';
 
-describe("normalize module", () => {
-  test("adds 1 + 2 to equal 3", () => {
-    expect(1 + 2).toBe(3);
+describe('normalize arguments', () => {
+  describe('correct replace', () => {
+    test('simple unix like argument', () => {
+      const commandArgument = ['argument=$UNIX_ARG'];
+      const env = { UNIX_ARG: 'unix_arg_value' };
+
+      const [normalizedArgument] = normalize(commandArgument, env);
+
+      expect(normalizedArgument).toBe('argument=unix_arg_value');
+    });
+
+    test('simple windows like argument', () => {
+      const commandArgument = ['argument=%WINDOWS_ARG%'];
+      const env = { WINDOWS_ARG: 'windows_arg_value' };
+
+      const [normalizedArgument] = normalize(commandArgument, env);
+
+      expect(normalizedArgument).toBe('argument=windows_arg_value');
+    });
+  });
+
+  test('prevent partial replacement', () => {
+    const commandArgument = ['aaa=$AAA', 'aa=$AA', 'a=$A'];
+    const env = {
+      A: 'a_arg_value',
+      AA: 'aa_arg_value',
+      AAA: 'aaa_arg_value',
+    };
+
+    const [arg1, arg2, arg3] = normalize(commandArgument, env);
+
+    expect(arg1).toBe('aaa=aaa_arg_value');
+    expect(arg2).toBe('aa=aa_arg_value');
+    expect(arg3).toBe('a=a_arg_value');
   });
 });
