d91f9158e328edc8f793b33a2aa6ba794b3511dc
Roman Astashevich <irqmen@gmail.com>
2024-05-03 13:28:00 +0300

    Configure test environment
    - Cleanup dev dependencies
    - Add dev dependencies for testing with initial config
    - Add npm script for running tests

---
+++ src/normalize.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+import { normalize } from "./normalize";
+
+describe("normalize module", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
