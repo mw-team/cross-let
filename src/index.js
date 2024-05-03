36968a6cf87faf4e6c88cbb94cae5d93ba0f2c75
Roman Astashevich <irqmen@gmail.com>
2024-05-03 14:19:34 +0300

    Configure sonarqube quality management tool for the workspace

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node --harmony
+
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 'use strict';
 
 import { exec } from 'child_process';
@@ -11,7 +31,7 @@ if (args.length === 1) {
   const [command] = normalize(args, process.env);
   const proc = exec(command, (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      console.error(`exec error:`, error);
       return;
     }
     process.stdout.write(stdout);
--- src/normalize.js
+++ src/normalize.js
@@ -1,13 +1,35 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 'use strict';
 
-export function normalize(args, envVariables) {
-  return args.map((arg) => {
+const normalize = (args, envVariables) => {
+  return args.map(arg => {
     Object.keys(envVariables)
-      .sort((x, y) => y.length - x.length) // NOTE: sort by descending length to prevent partial replacement
-      .forEach((key) => {
+      // NOTE: sort by descending length to prevent partial replacement
+      .sort((x, y) => y.length - x.length)
+      .forEach(key => {
         const regex = new RegExp(`\\$${key}|%${key}%`, 'ig');
         arg = arg.replace(regex, envVariables[key]);
       });
     return arg;
   });
-}
+};
+
+export { normalize };
